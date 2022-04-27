af0b47447f6e750fa1c344d831c9d423c5d42ad2
Vinicius Maciel <vinicius.code@outlook.com>
2022-04-27 15:09:11 -0300

    18

---
+++ webpack/webpack.config.js
@@ -0,0 +1,43 @@
+const modoDev = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+    mode: modoDev ? 'development' : 'production',
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    devServer: {
+        open: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader', // interpreta @import, url()...
+                'sass-loader'
+            ]
+        }]
+    }
+}
