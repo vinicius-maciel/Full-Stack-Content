c6b8fa8bdaafff4b6b751625343f7df1047d00dd
Vinicius Maciel <vinicius.code@outlook.com>
2022-05-05 23:50:21 -0300

    Registration-system-basic

---
+++ react/crud/frontend/src/components/template/Nav.jsx
@@ -0,0 +1,17 @@
+import './Nav.css'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default props =>
+    <aside className="menu-area">
+        <nav className="menu">
+
+            {/* Refatorar  navItem.jsx*/}
+            <Link to="#/">
+                <i className="fa fa-home"></i> Inicio
+            </Link>
+            <Link to="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </Link>
+        </nav>
+    </aside>
